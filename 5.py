d437bed3cf642ae27e17a1715d665815a9fcd9c6
Parthnvm <dedtrek94@gmail.com>
2026-01-28 12:23:31 +0530

    Refactor input handling to streamline integer retrieval

---
+++ 5.py
@@ -0,0 +1,43 @@
+# gets a number from the user
+
+# x = int(input("Whats x?:"))
+# print(f"x is {x}")
+# while True:
+#     try:
+#         y = int(input("Whats y?: "))
+    
+#     except:
+#         print("That is not a valid integer.")
+#     else:
+#         print(f"y is {y}")
+#         break
+    
+# def main():
+#     x = get_int()
+#     print(f"x is {x}")
+    
+# def get_int():
+#     while True:
+#         try:
+#             x = int(input("Whats x?: "))
+#         except:
+#             print("That is not a valid integer.")
+#         else:
+#             return x
+
+# main()
+
+def main():
+    x = get_int()
+    print(f"x is {x}")
+    
+def get_int():
+    while True:
+        try:
+            return int(input("Whats x?: "))
+        except:
+            pass
+
+main()
+    
+    
