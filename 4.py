427e21a79daf103c4fc356d43ff5631b43cc9e25
Parthnvm <dedtrek94@gmail.com>
2026-01-28 10:37:03 +0530

    Implement email validation regex for user input

--- 4.py
+++ 4.py
@@ -2,4 +2,7 @@ import re
 
 email = input("Enter your email address: ").strip()
 
-if re.search("@", email) 
+if re.search(r"^[\w.-]+@[\w.-]+\.(edu|com|org)$", email): 
+    print("Valid email")
+else:
+    print("Invalid email")
