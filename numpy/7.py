7c57e66c07446225906274bd4bf937f851963b75
Parthnvm <dedtrek94@gmail.com>
2026-01-29 10:09:10 +0530

    Refactor numpy example: add array initialization and aggregation functions

--- numpy/7.py
+++ numpy/7.py
@@ -2,4 +2,20 @@
 
 import numpy as np
 
-np.array([1, 2, 3, 4, 5])
+# np.array([1, 2, 3, 4, 5])
+
+a = np.array([1, 2, 3])
+
+# print(a.dtype)
+
+# d = np.float32(1.0)
+# print(d.dtype)
+
+# Agrregations 
+sum = np.sum(a)
+print(sum)
+
+print(np.min(a))
+print(np.max(a))
+print(np.mean(a))
+print(np.std(a))
