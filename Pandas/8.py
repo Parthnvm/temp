d86525b3bd6623d7e7fa26dfb2b3d14221ab1222
Parthnvm <dedtrek94@gmail.com>
2026-01-29 12:02:01 +0530

    Add a new script to plot Sachin Tendulkar's yearly runs using Matplotlib
    - Created a new file `mat_plotlib.py` in the `matplot` directory.
    - Implemented a line plot to visualize the runs scored by Sachin Tendulkar from 1990 to 2010.
    - Added labels for the x-axis and y-axis, along with a title for the plot.

--- Pandas/8.py
+++ Pandas/8.py
@@ -1,1 +1,28 @@
 #Pandas
+
+import pandas as pd
+
+s1 = pd.Series([10,20,30,40,50,60,70,80,90,100])
+# print(s1)
+
+# print(s1.head()) # .head shows the 1st 5 rows by default
+# print(s1.head(1)) # shows the 1st row only
+
+# df = pd.DataFrame(
+#     {
+#         "id": [1,2,3,4,5],
+#         "name": ["Parth","Atharva","Sumit","Rohit","Ankit"],
+#         "age": [20,21,22,23,24]
+#     }
+    
+# )
+
+
+
+# df.info()
+
+
+# print(df[['name','age']])
+
+df = pd.read_csv("Pandas/temp.csv")
+print(df.head())
